21efba219860e810cbff69ec11079744c4297b4d
SevanandYadav <sevanandyadav@gmail.com>
2025-08-17 14:14:45 +0530

    hot-start

---
+++ src/main/java/com/arsenal/ecomm/shopperstop/controller/HealthController.java
@@ -0,0 +1,18 @@
+package com.arsenal.ecomm.shopperstop.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    
+    @GetMapping("/health")
+    public String health() {
+        return "OK";
+    }
+    
+    @GetMapping("/ping")
+    public String ping() {
+        return "pong";
+    }
+}
